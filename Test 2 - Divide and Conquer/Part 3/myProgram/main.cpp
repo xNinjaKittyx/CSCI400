3cd308d9459206c1f1e82c35fd44a2c5e927284d
Daniel Ahn <sangbumahn@gmail.com>
2015-11-05 16:14:32 -0800

    Finished Part 3 and 4.

--- Test 2 - Divide and Conquer/Part 3/myProgram/main.cpp
+++ Test 2 - Divide and Conquer/Part 3/myProgram/main.cpp
@@ -13,16 +13,140 @@
 using namespace std;	//declare access to standard stuff like cin, cout
 
 
+
+int mergeSort(vector<int> &A, vector<int> &B, vector<int> &C) {
+	int count = 0;
+	int i = 0, j = 0;
+	// Count Significant Inversions
+	while (i < A.size() && j < B.size()) {
+		if (A[i] > 2 * B[j]) {
+			count += A.size() - 1;
+			j++;
+		}
+		else {
+			i++;
+		}
+	}
+	i = 0, j = 0;
+
+	int cpointer = 0;
+	// Sort the Array.
+	while (i < A.size() && j < B.size()) {
+		if (A[i] < B[j]) {
+
+			C[cpointer] = A[i];
+			i++;
+		}
+		else {
+			C[cpointer] = B[j];
+			j++;
+		}
+		cpointer++;
+	}
+	while (i < A.size()) {
+		C[cpointer] = A[i];
+		i++;
+		cpointer++;
+	}
+	while (j < B.size()) {
+		C[cpointer] = B[j];
+		j++;
+		cpointer++;
+	}
+
+	return count;
+	
+}
+
+int countInversions(vector<int> &A) {
+	int size = A.size();
+
+	// If the vector is only 1, there is no inversions to be counted.
+	if (size <= 1) {
+		return 0;
+	}
+	else if (size == 2) {
+		if (A[0] > 2 * A[1]) {
+			if (A[0] > A[1]) {
+				int temp = A[0];
+				A[0] = A[1];
+				A[1] = temp;
+			}
+			return 1;
+		}
+		else
+			if (A[0] > A[1]) {
+				int temp = A[0];
+				A[0] = A[1];
+				A[1] = temp;
+			}
+			return 0;
+	}
+	else {
+
+		// First we need to split the vector into two parts.
+		vector<int> B;
+		vector<int> C;
+
+		// Now we need to know how much to put in each vector.
+		int bsize = A.size() / 2;
+		int csize = A.size() - bsize;
+
+		// Now resize the vectors.
+		B.resize(bsize);
+		C.resize(csize);
+
+		// Now we need to put all the values of A into these two vectors.
+		for (int i = 0; i < bsize; i++) { // Reasons for using the csize/bsize, is to reduce number of vector calls.
+			B[i] = A[i];
+
+		}
+		for (int i = 0; i < csize; i++) {
+			C[i] = A[bsize + i];
+		}
+
+
+		int result = 0;
+		result += countInversions(B);
+		result += countInversions(C);
+		result += mergeSort(B, C, A);
+
+		return result;
+
+
+	}
+}
+
 int main()
 
 {
 
 	srand(time(NULL));
 
+
+	// First we will create an Array A of elements n.
+	cout << "How many elements?\n";
+	int elements;
+	cin >> elements;
+
+	vector<int> A;
+	for (int i = 0; i < elements; i++) {
+		cout << "A[" << i << "]: ";
+		int asdf;
+		cin >> asdf;
+		A.push_back(asdf);
+	}
 	
 	clock_t timer;
 	timer = clock();
 	
+	int total = countInversions(A);
+
+	cout << "There are " << total << " significant inversions" << endl;
+
+	for (int i = 0; i < A.size(); i++) {
+		cout << "Results[" << i << "]: " << A[i] << endl;
+	}
 
 
 
--- Test 2 - Divide and Conquer/Part 4/myProgram/main.cpp
+++ Test 2 - Divide and Conquer/Part 4/myProgram/main.cpp
@@ -12,6 +12,75 @@
 #include <time.h>
 using namespace std;	//declare access to standard stuff like cin, cout
 
+bool found = false;
+
+vector<int> gradientSearch(vector<vector<int>> &A, vector<int> selected) {
+	// Assumes that columns and Rows are EQUAL
+
+	int size = A[0].size();
+
+	int x = selected[0];
+	int y = selected[1];
+
+	// Check for lowest number
+	int lowestNumber = A[x][y];
+
+	bool up = true;
+	bool down = true;
+	bool left = true;
+	bool right = true;
+
+	if (x == 0)
+		left = false;
+	if (x == size - 1)
+		right = false;
+	if (y == 0)
+		down = false;
+	if (y == size - 1)
+		up = false;
+	
+	bool auth = true;
+	if (up) {
+		if (A[x][y + 1] < A[x][y]) {
+			auth = false;
+			selected = gradientSearch(A, { x, y + 1 });
+			if (found)
+				return selected;
+		}
+	}
+	if (down) {
+
+		if (A[x][y - 1] < A[x][y]) {
+			auth = false;
+			selected = gradientSearch(A, { x, y - 1 });
+			if (found)
+				return selected;
+		}
+	}
+	if (left) {
+		if (A[x - 1][y] < A[x][y]) {
+			auth = false;
+			selected = gradientSearch(A, { x - 1, y });
+			if (found)
+				return selected;
+		}
+	}
+	if (right) {
+		if (A[x + 1][y] < A[x][y]) {
+			auth = false;
+			selected = gradientSearch(A, { x + 1, y });
+			if (found)
+				return selected;
+		}
+	}
+
+	if (auth)
+		found = true;
+	return selected;
+
+	
+	
+}
 
 int main()
 
@@ -20,13 +89,42 @@ int main()
 	srand(time(NULL));
 
 	
-	clock_t timer;
-	timer = clock();
+
+	cout << "How many columns and rows?\n";
+	int elements;
+	cin >> elements;
+
+	vector<vector<int>> A;
+	A.resize(elements);
+	for (int i = 0; i < elements; i++) {
+		for (int j = 0; j < elements; j++) {
+			cout << "A[" << i << "][" << j << "]: ";
+			int asdf;
+			cin >> asdf;
+			A[i].push_back(asdf);
+		}
+	}
 	
 
+	clock_t timer;
+	timer = clock();
 
 
+	// Start from the top left corner, then move if there's a number lower.
 
+
+	vector<int> selected = gradientSearch(A, { 0, 0 });
+
+	cout << "Local Minimum was found at " << selected[0] << ", " << selected[1] << endl;
+
+	cout << endl << "This graph uses X as left to right, and Y as up to down." << endl << endl; 
+
+	for (int j = 0; j < elements; j++) {
+		for (int i = 0; i < elements; i++) {
+			cout << A[i][j] << " ";
+		}
+		cout << endl;
+	}
 
 
 	timer = clock() - timer;
