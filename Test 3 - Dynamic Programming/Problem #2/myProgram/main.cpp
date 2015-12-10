bcdcb4277583356ed48e9e78d7148f837679be8d
Daniel Ahn <sangbumahn@gmail.com>
2015-12-09 17:42:13 -0800

    Problem #2

--- Test 3 - Dynamic Programming/Problem #2/myProgram/main.cpp
+++ Test 3 - Dynamic Programming/Problem #2/myProgram/main.cpp
@@ -37,12 +37,83 @@ int main()
 	
 	*/
 
+
+	cout << "How much does your postage cost?" << endl;
+	int cost;
+	cin >> cost;
+	while (cost < 1) {
+		cout << "Invalid postage cost." << endl;
+		cout << "Type another cost." << endl;
+		cin >> cost;
+	}
+
+
 	clock_t timer;
 	timer = clock();
+	vector<vector<int>> listOfCosts(3, vector<int>(cost+1)); // First number is the stamp type (1,4,5) and the second one is to keep track of all costs.
+	for (int i = 0; i < 3; i++) {
+		listOfCosts[i][0] = 0; // 0 should be 0. Cost of a 0 postage stamp, should be 0.
+	}
+	listOfCosts[0][1] = 1; // 1 cent postage has... 1 cent stamp
+	listOfCosts[1][1] = 0; // 0 4 cent stamps
+	listOfCosts[2][1] = 0; // 0 5 cent stamps.
+
+	// I could just check 1, 4, 5 behind the current option to see which one will give the best solution.
+	for (int i = 2; i <= cost; i++) { // the one that we're editing
+		
+		int totalStampsA = listOfCosts[0][i - 1] + listOfCosts[1][i - 1] + listOfCosts[2][i - 1];
+		int totalStampsB;
+		int totalStampsC;
+		if (i > 3) {
+			totalStampsB = listOfCosts[0][i - 4] + listOfCosts[1][i - 4] + listOfCosts[2][i - 4];
+		}
+		if (i > 4) {
+			totalStampsC = listOfCosts[0][i - 5] + listOfCosts[1][i - 5] + listOfCosts[2][i - 5];
+		}
+
+		if (totalStampsA < totalStampsB) {
+			if (totalStampsA < totalStampsC) {
+				// if totalStampsA is the least amount of stamps
+				listOfCosts[0][i] = listOfCosts[0][i - 1] + 1;
+				listOfCosts[1][i] = listOfCosts[1][i - 1];
+				listOfCosts[2][i] = listOfCosts[2][i - 1];
+			}
+			else {
+				// if totalStampsC is the least amount of stamps
+				listOfCosts[0][i] = listOfCosts[0][i - 5];
+				listOfCosts[1][i] = listOfCosts[1][i - 5];
+				listOfCosts[2][i] = listOfCosts[2][i - 5] + 1;
+			}
+		}
+		else {
+			if (totalStampsB < totalStampsC) {
+				// if totalStampsB is the least amount of stamps
+				listOfCosts[0][i] = listOfCosts[0][i - 4];
+				listOfCosts[1][i] = listOfCosts[1][i - 4] + 1;
+				listOfCosts[2][i] = listOfCosts[2][i - 4];
+			}
+			else {
+				// if totalStampsC is the least amount of stamps
+				listOfCosts[0][i] = listOfCosts[0][i - 5];
+				listOfCosts[1][i] = listOfCosts[1][i - 5];
+				listOfCosts[2][i] = listOfCosts[2][i - 5] + 1;
+			}
+		}
+	}
+	
+	timer = clock() - timer;
+
+	ifstream fin;
+	ofstream fout;
+	fout.open("output.txt");
+	fout << "\tCosts\t1Cent\t4Cent\t5Cent" << endl;
+	for (int i = 0; i <= cost; i++) {
+		fout << "\t" << i << "\t" << listOfCosts[0][i] << "\t" << listOfCosts[1][i] << "\t" << listOfCosts[2][i] << endl;
+	}
 
 
-	//fout << "This took " << (((float)timer) / CLOCKS_PER_SEC) << " seconds" << endl;
-	//fout.close();
+	fout << "This took " << (((float)timer) / CLOCKS_PER_SEC) << " seconds" << endl;
+	fout.close();
 
 
 	cout << "All results have been saved to output.txt." << endl;
